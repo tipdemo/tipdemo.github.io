d2a0285eeec5e60a73831a29fef748feccd5b1a5
GijsvandenBeucken <55687057+GijsvandenBeucken@users.noreply.github.com>
2024-11-08 10:31:23 +0100

    contact toegevoegd

--- navbar.js
+++ navbar.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
       { href: "/index.html", icon: "fas fa-home", text: "Home", ariaLabel: "Home" },
       { href: "/examples/index.html", icon: "fas fa-lightbulb", text: "Voorbeelden", ariaLabel: "Voorbeelden" },
       { href: "/research/index.html", icon: "fas fa-flask", text: "Onderzoek", ariaLabel: "Onderzoek" },
+      { href: "/contact/index.html", icon: "fas fa-comments", text: "Contact en feedback", ariaLabel: "Contact en feedback" }
     /*   { href: "/requirements/index.html", icon: "fas fa-tasks", text: "Randvoorwaarden", ariaLabel: "Randvoowaarden" }, */
     ];
     
