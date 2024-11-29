d82649e23b9cd50c893d6b81541a5604122c792e
GijsvandenBeucken <55687057+GijsvandenBeucken@users.noreply.github.com>
2024-11-29 18:15:23 +0100

    agreements

--- research/agreements/demo.js
+++ research/agreements/demo.js
@@ -1,5 +1,5 @@
 const demoConfig = {
-    totalSteps: 9 // Definieer hier het aantal stappen voor deze demo
+    totalSteps: 11 // Definieer hier het aantal stappen voor deze demo
 };
 
 let currentStep = 1; // Houd de huidige stap bij
