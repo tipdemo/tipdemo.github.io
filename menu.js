cf2989797c90ec4e937dcd4a4fc5b498ff7f8649
Gijs <gijs@gijsvandenbeucken.nl>
2024-12-09 13:54:26 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -22,19 +22,26 @@ document.addEventListener("DOMContentLoaded", () => {
       console.error("Navbar-logo ontbreekt in de DOM.");
     }
 
-    
-    // Links configuratie
-    const links = [
-      { href: "https://www.tipdemo.nl/index.html", icon: "fas fa-home", text: "Home" },
-      { href: "https://www.tipdemo.nl/examples/index.html", icon: "fas fa-play", text: "Demo's" },
-      { href: "https://www.tipdemo.nl/research/index.html", icon: "fas fa-flask", text: "Onderzoek" },
-      { href: "https://www.tipdemo.nl/about/index.html", icon: "fas fa-info-circle", text: "Over" },
-      { href: "https://www.tipdemo.nl/contact/index.html", icon: "fas fa-comments", text: "Contact" },
-    ];
+// Links configuratie
+const links = [
+    { href: "https://www.tipdemo.nl/index.html", icon: "fas fa-home", text: "Home" },
+    { href: "https://www.tipdemo.nl/examples/index.html", icon: "fas fa-play", text: "Demo's" },
+    { href: "https://www.tipdemo.nl/research/index.html", icon: "fas fa-flask", text: "Onderzoek" },
+    { hr: true }, // Voeg een scheidingslijn toe
+    { href: "https://www.tipdemo.nl/about/index.html", icon: "fas fa-info-circle", text: "Over" },
+    { href: "https://www.tipdemo.nl/contact/index.html", icon: "fas fa-comments", text: "Contact" },
+  ];
   
-    // Dynamisch links genereren
-    if (menuLinksContainer) {
-      links.forEach((link) => {
+  // Dynamisch links genereren
+  if (menuLinksContainer) {
+    links.forEach((link) => {
+      if (link.hr) {
+        // Voeg een scheidingslijn toe
+        const hr = document.createElement("hr");
+        hr.className = "nav-separator"; // Optionele klasse voor styling
+        menuLinksContainer.appendChild(hr);
+      } else {
+        // Voeg een link toe
         const li = document.createElement("li");
         li.innerHTML = `
           <a href="${link.href}">
@@ -42,10 +49,11 @@ document.addEventListener("DOMContentLoaded", () => {
           </a>
         `;
         menuLinksContainer.appendChild(li);
-      });
-    } else {
-      console.error("Menu-links container ontbreekt in de DOM.");
-    }
+      }
+    });
+  } else {
+    console.error("Menu-links container ontbreekt in de DOM.");
+  }
   
  // Menu functionaliteit
  if (hamburgerMenu && fullscreenMenu && closeMenu) {
