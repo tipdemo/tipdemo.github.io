dd9c36593766e99b9aab5e79e7addcdacda5cf62
GijsvandenBeucken <55687057+GijsvandenBeucken@users.noreply.github.com>
2024-12-14 13:25:28 +0100

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -33,20 +33,21 @@ router.get('/data/:id', (req, res) => {
 
 // GET endpoint voor QR-code genereren
 router.get('/qrcode/:id', async (req, res) => {
-  const id = req.params.id;
-  if (!dataStore[id]) {
-    return res.status(404).send('Data not found for QR code');
-  }
-
-  // Maak een API-URL die verwijst naar het data endpoint
-  const apiUrl = `${req.protocol}://${req.get('host')}/api/data/${id}`;
-
-  try {
-    const qrCodeDataURL = await QRCode.toDataURL(apiUrl);
-    res.send(`<img src="${qrCodeDataURL}" alt="QR Code">`);
-  } catch (err) {
-    res.status(500).send('Error generating QR code');
-  }
-});
+    const id = req.params.id;
+    if (!dataStore[id]) {
+      return res.status(404).send('Data not found for QR code');
+    }
+  
+    // Maak een API-URL die verwijst naar het data endpoint
+    const apiUrl = `${req.protocol}://${req.get('host')}/api/data/${id}`;
+  
+    try {
+      res.setHeader('Content-Type', 'image/png'); // Content-Type voor PNG instellen
+      await QRCode.toFileStream(res, apiUrl); // Stream de QR-code direct als PNG
+    } catch (err) {
+      res.status(500).send('Error generating QR code');
+    }
+  });
+  
 
 module.exports = router;
