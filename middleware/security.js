0d7709a5e402ebbdc88e705f5655d1df57ab9ad3
GijsvandenBeucken <55687057+GijsvandenBeucken@users.noreply.github.com>
2024-12-14 12:11:16 +0100

    Update security.js

--- middleware/security.js
+++ middleware/security.js
@@ -2,8 +2,10 @@
 const helmet = require('helmet');
 
 const securityMiddleware = [
-  // Algemene beveiliging met Helmet
-  helmet(),
+  // Algemene beveiliging met Helmet, disable crossOriginEmbedderPolicy
+  helmet({
+    crossOriginEmbedderPolicy: false
+  }),
 
   // X-Frame-Options
   helmet.frameguard({
@@ -30,11 +32,15 @@ const securityMiddleware = [
       ],
       imgSrc: [
         "'self'",
-        "data:"
+        "data:",
+        // Voeg jouw specifieke afbeeldings-CDN toe indien nodig, bijvoorbeeld:
+        // "https://your-image-cdn.com"
       ],
       fontSrc: [
         "'self'",
-        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/webfonts/fa-solid-900.woff2"
+        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/webfonts/fa-solid-900.woff2",
+        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/webfonts/fa-brands-400.woff2",
+        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/webfonts/fa-brands-400.ttf"
       ],
       connectSrc: ["'self'"],
       objectSrc: ["'none'"],
